fd941093988cb56a2064f2b9c54d0fe464900cad
DIvyanshuddev <divyanshu311098@gmail.com>
2025-04-09 16:02:48 -0700

    change the icons

--- src/Components/Theme.jsx
+++ src/Components/Theme.jsx
@@ -4,21 +4,22 @@ import BedtimeOutlinedIcon from '@mui/icons-material/BedtimeOutlined';
 import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
 export default function Theme(props) {
 
-  const [theme,setTheme]=useState("light")
-  useEffect(()=>{
+  const [theme, setTheme] = useState("light")
+  useEffect(() => {
     props.handleTheme(theme)
-  },[theme])
+  }, [theme])
   return (
     <Stack direction={'row'} alignItems={'center'}>
       {
-        theme==="dark"?(
-          <IconButton onClick={()=>setTheme('light')}>
-            <WbSunnyOutlinedIcon style={{color:"yellow"}} fontSize='large' />
+        theme === "dark" ? (
+          <IconButton onClick={() => setTheme('light')}>
+            <BedtimeOutlinedIcon style={{ color: "gray" }} fontSize='large' />
+
           </IconButton>)
-         :(
-           <IconButton onClick={()=>setTheme('dark')}>
-               <BedtimeOutlinedIcon style={{color:"gray"}} fontSize='large' />
-           </IconButton>)
+          : (
+            <IconButton onClick={() => setTheme('dark')}>
+              <WbSunnyOutlinedIcon style={{ color: "yellow" }} fontSize='large' />
+            </IconButton>)
       }
     </Stack>
   )
